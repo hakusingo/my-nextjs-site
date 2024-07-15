efba8e60e4c38a6ede82e285705284c41ffb1b50
hakusingo <hakusingo@gmail.com>
2024-07-15 13:47:01 +0900

    first

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+// app/page.tsx
 import { gql } from '@apollo/client';
 import client from '../lib/apolloClient';
 
@@ -19,7 +20,7 @@ export default async function Home() {
 
   return (
     <div>
-      <h1>Blog Posts</h1>
+      <h1>ブログの投稿です</h1>
       {data.posts.nodes.map((post: any, index: number) => (
         <div key={index}>
           <h2>{post.title}</h2>
